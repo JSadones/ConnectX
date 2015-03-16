c9a4ddb384f2f20adf02575f59cfec6ecf11ab75
Jel Sadones <jel.sadones@student.howest.be>
2015-03-16 14:01:51 +0100

    Aanmaak project

---
+++ XOPeenRijLibrary/XOPeenRijUnitTest/XOPeenRijTester.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using XOPeenRijLibrary;
+
+namespace XOPeenRijUnitTest
+{
+    [TestClass]
+    public class XOPeenRijTester
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
