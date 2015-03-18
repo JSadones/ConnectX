14035cb1897e596865da33fbd13810587f8cbc88
Jel Sadones <jel.sadones@student.howest.be>
2015-03-18 19:48:20 +0100

    Update AssemblyInfo.cs

--- Windows Client/ConnectX/ConnectXLibrary/Properties/AssemblyInfo.cs
+++ Windows Client/ConnectX/ConnectXLibrary/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("XOPeenRijLibrary")]
+[assembly: AssemblyTitle("ConnectXLibrary")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("XOPeenRijLibrary")]
+[assembly: AssemblyProduct("ConnectXLibrary")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
