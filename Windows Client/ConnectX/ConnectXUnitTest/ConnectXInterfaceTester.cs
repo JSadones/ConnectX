4a5e8da022382671046b2967b1b43ba849df6241
Shane Deconinck <shane.deconinck@gmail.com>
2015-03-19 10:23:48 +0100

    New tests for interface

--- Windows Client/ConnectX/ConnectXUnitTest/ConnectXInterfaceTester.cs
+++ Windows Client/ConnectX/ConnectXUnitTest/ConnectXInterfaceTester.cs
@@ -26,5 +26,32 @@ namespace ConnectXUnitTest
             // Parameter == player number
             Assert.IsTrue(gameInterface.getScore(1) == 1);
         }
+
+        [TestMethod]
+        public void TestNewGame2HumanPlayersWith9RowsAnd11Columns()
+        {
+            Assert.IsFalse(gameInterface.gameRunning());
+            gameInterface.newGame(2, 9, 11);
+            Assert.IsTrue(gameInterface.gameRunning());
+        }
+
+        [TestMethod]
+        public void TestGetPlayerAtPlay()
+        {
+            gameInterface.setPlayerAtPlay(1);
+            // Parameter == player number
+            Assert.IsTrue(gameInterface.getPlayerAtPlay() == 1);
+        }
+
+        [TestMethod]
+        public void TestInsertTokenInColumn2ByPlayer1()
+        {
+            gameInterface.setPlayerAtPlay(1);
+            
+            // insertToken returns true if token is successfully inserted
+            Assert.IsTrue(gameInterface.insertToken(2,1));
+            // return false if it is not his turn
+            Assert.IsFalse(gameInterface.insertToken(2, 2));
+        }
     }
 }
