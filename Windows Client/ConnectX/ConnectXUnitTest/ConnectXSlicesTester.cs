b8c21a857ecda99d44cdce2b502fddb373cca639
Shane Deconinck <shane.deconinck@gmail.com>
2015-03-19 15:57:12 +0100

    TestSlices

--- Windows Client/ConnectX/ConnectXUnitTest/ConnectXSlicesTester.cs
+++ Windows Client/ConnectX/ConnectXUnitTest/ConnectXSlicesTester.cs
@@ -1,14 +1,43 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConnectXLibrary;
 
 namespace ConnectXUnitTest
 {
     [TestClass]
-    public class UnitTest1
+    public class ConnectXSlicesTester
     {
-        [TestMethod]
-        public void TestMethod1()
+        ConnectXInterface gameInterface;
+
+        [TestInitialize]
+        public void setup()
         {
+        }
+
+        [TestMethod]
+        public void TestSlice1_1NamenOpvragen()
+        {
+            string naamSpeler1 = "Speler 1";
+            string naamSpeler2 = "Speler 2";
+
+            gameInterface = new ConnectXInterface(naamSpeler1, naamSpeler2);
+
+            Assert.IsTrue(gameInterface.getName(1) == "Speler 1");
+            Assert.IsTrue(gameInterface.getName(2) == "Speler 2");
+            Assert.IsFalse(gameInterface.getName(2) == "Speler 1");
+        }
+
+        [TestMethod]
+        public void TestSlice1_2NamenOpvragen()
+        {
+            string naamSpeler1 = "Speler 1";
+            string naamSpeler2 = "Speler 2";
+
+            gameInterface = new ConnectXInterface(naamSpeler1, naamSpeler2);
+
+            Assert.IsTrue(gameInterface.getName(1) == "Speler 1");
+            Assert.IsTrue(gameInterface.getName(2) == "Speler 2");
+            Assert.IsFalse(gameInterface.getName(2) == "Speler 1");
         }
     }
 }
