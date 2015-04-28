a682d6f8153e40eaade7f3af4529a61bf6471bff
Shane Deconinck <shane.deconinck@gmail.com>
2015-04-28 14:01:25 +0200

    removed script tags from webClient.js

---
+++ Webclient/Webpage without client/js/webclient.js
@@ -0,0 +1,75 @@
+        $(document).ready(function(){   
+        // Start
+            // Show start div
+        // Options
+            // Show options div
+             $( "#play-vs-cpu" ).click(function() {
+                $('#start').hide();
+                $( "#options" ).show();
+
+            });
+
+            function getSecondClass(thisObj) {
+                var counter = 0;
+                var classes = {};
+                $(thisObj.attr('class').split(' ')).each(function() { 
+                    if (this !== '') {
+                        classes[counter] = this;
+                    }    
+                    counter++;
+                });
+                return classes[1];
+             }
+
+             function insertToken(column) {
+                //
+             }
+           $(document).on("mouseenter", ".column", function() {
+                $('.'+getSecondClass($(this)).toString()).css("background-color","red");
+            });
+
+            $(document).on("mouseleave", ".column", function() {
+                $('.'+getSecondClass($(this)).toString()).css("background-color","transparent");
+            });
+
+            $(document).on("click", ".column", function() {
+                var column = getSecondClass($(this)).replace(/\D/g,'');
+                insertToken(column);
+                //updateRaster();
+            });
+        });
+
+        function submitForm() {
+
+
+                var $inputs = $('#form :input');
+
+                var values = {};
+                $inputs.each(function() {
+                    values[this.name] = $(this).val();
+                });
+
+                var content = "<table width='70%'><tr id='selectie'>";
+
+                for (var i = 0; i < values["columns"]; i++) {
+                    content += "<th>o</th>";
+                }
+                    
+                content += '</tr>';
+                for (var i = 0; i< values["rows"]; i++) {
+                    content += '<tr>';
+                    for (var j = 0; j < values["columns"]; j++) {
+                        content += "<td class='column column"+j+"'>x</td>"
+                    }
+                    content += '</tr>';
+                }
+                content += '</table>';
+                $( "#game" ).html(content);
+
+                $('#options').hide();
+
+                $( "#game" ).show();
+
+
+            return false;
+        }
