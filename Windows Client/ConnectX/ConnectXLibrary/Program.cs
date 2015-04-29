39e015a184b70a77866efef8385d7520fe0bbf8f
Jel Sadones <jel.sadones@student.howest.be>
2015-04-29 09:35:13 +0200

    abcd

--- Windows Client/ConnectX/ConnectXLibrary/Program.cs
+++ Windows Client/ConnectX/ConnectXLibrary/Program.cs
@@ -14,7 +14,6 @@ namespace ConnectXLibrary
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            Application.Run(new Server());
             Application.Run(new Menu());
         }
     }
