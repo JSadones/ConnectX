30f2922813841183181dbf6df817cae522da7870
Shane Deconinck <shane.deconinck@gmail.com>
2015-05-13 12:49:39 +0200

    server fix

--- Windows Client/ConnectX/ConnectXLibrary/Server.cs
+++ Windows Client/ConnectX/ConnectXLibrary/Server.cs
@@ -35,7 +35,7 @@ namespace ConnectXLibrary
 
                 listener.Start();
 
-                this.listenThread = new Thread(new ParameterizedThreadStart(startlistener));
+                listenThread = new Thread(new ParameterizedThreadStart(startlistener));
                 listenThread.Start();
                 Process.Start(Path.Combine(Directory.GetCurrentDirectory(), "../../../../../Webclient/index.html"));
             }
@@ -205,6 +205,7 @@ namespace ConnectXLibrary
         }
 		private void btnStopServer_Click(object sender, EventArgs e)
         {
+            listenThread.Abort();
             listener.Stop();
             listener.Close();
             this.Close();
