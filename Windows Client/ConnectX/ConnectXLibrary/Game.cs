1573f8c07ee9613936d73403faefe775daa18049
Jel Sadones <jel.sadones@student.howest.be>
2015-05-15 00:55:17 +0200

    token in aparte classe

--- Windows Client/ConnectX/ConnectXLibrary/Game.cs
+++ Windows Client/ConnectX/ConnectXLibrary/Game.cs
@@ -342,65 +342,7 @@ namespace ConnectXLibrary
         private void calculateSlotSize()
         {
             size = 480 / rows;
-        }
-        #endregion
-    }
-
-    class token : PictureBox
-    {
-        #region State
-        Timer t;
-        Random r = new Random();
-        int row, rows, size, startHeight;
-        bool completed = false;
-        #endregion
-
-        #region Constructor
-        public token()
-        {
-            move();
-        }//token
-        #endregion
-
-        #region Methods
-        public void create(int player, int size, Point location, Panel pnlGame, int row, int rows, int startHeight)
-        {
-            this.row = row;
-            this.rows = rows;
-            this.size = size;
-            this.startHeight = startHeight;
-
-            this.Parent = pnlGame;
-            this.Location = location;
-            this.Size = new Size(size, size);
-            this.BackColor = Color.Transparent;
-            this.BackgroundImageLayout = ImageLayout.Stretch;
-
-            if (player == 1) this.BackgroundImage = Resources.blueToken;
-            else this.BackgroundImage = Resources.redToken;
-            while(completed){
-                System.Diagnostics.Debug.WriteLine("KANKERHOMOS");
-            }
-        }//create
-
-        private void move()
-        {
-            t = new Timer();
-            t.Interval = 10;
-
-            t.Tick += new EventHandler(t_Tick);
-
-            t.Start();
-        }//move
-
-        private void t_Tick(object sender, EventArgs e)
-        {
-            this.Location += new Size(0, 5);
-            if (this.Location.Y > ((rows - row - 1) * size) + startHeight) {
-                completed = !completed;
-                t.Stop();
-            }
-        }//t_Tick
+        }//calculateSlotSize
         #endregion
     }
 }
